55f03cc27160ccd88773ffbaf75fe2ae4550bc7e
arjun.airtory <arjun12345krishna@gmail.com>
2024-07-13 20:53:37 +0530

    Init

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+
+const express = require('express');
+const router = express.Router();
+
+
+const api = require('./api');
+const auth = require('./auth');
+
+
+router.use('/api/v1', api);
+
+
+router.get('/', (req, res) => {
+    res.json({
+        status: 200,
+        message: 'API is working properly'
+    });
+});
+
+
+module.exports = router;
