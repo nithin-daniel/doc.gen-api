e7902906eab7716da6006f39e7528f50ab72d1b1
Nithin Daniel <nithindaniel2018@gmail.com>
2024-07-14 10:45:27 +0530

    feat:removed netlify

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 const express = require('express');
-const serverless = require("serverless-http");
+
 const app = express();
 app.disable('x-powered-by');
 
@@ -13,7 +13,7 @@ const routes = require('./routes');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/.netlify/functions/app", router);
+
 
 
 app.use('/', routes);
@@ -40,4 +40,3 @@ app.listen((process.env.PORT || 3000), () => {
 });
 
 
-module.exports.handler = serverless(app);
