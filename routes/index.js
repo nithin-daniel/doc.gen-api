a9792d1f8c14d91fc0425b8d4aa50745b55ecde2
Nithin Daniel <nithindaniel2018@gmail.com>
2024-08-21 14:42:11 +0530

    fix:updated index.js

--- routes/index.js
+++ routes/index.js
@@ -22,61 +22,6 @@ const s3 = new S3({
 });
 
 router.get("/", async (req, res) => {
-  // console.log(await s3.listBuckets().promise());
-  var axios = require("axios").default;
-  const fs = require("fs");
-  const path = require("path");
-  const objectKey = "images/output1.html";
-  var options = {
-    method: "PUT",
-    // url: `https://api.cloudflare.com/client/v4/accounts/${process.env.R2_DOCGEN_ACCOUNT_ID}/r2/buckets/docgen-backend`,
-    url: `https://api.cloudflare.com/client/v4/accounts/${
-      process.env.R2_DOCGEN_ACCOUNT_ID
-    }/r2/buckets/docgen-backend/objects/${encodeURIComponent("output1.html")}`,
-    headers: {
-      "Content-Type": "application/octet-stream",
-      Authorization: "Bearer " + process.env.R2_AUTH_TOKEN,
-    },
-    data: fs.createReadStream(path.join(__dirname, "../output.html")),
-  };
-
-  axios
-    .request(options)
-    .then(function (response) {
-      console.log(response.data.result);
-      // Generate the public URL
-      const publicUrl = `https://docgen-backend.${
-        process.env.R2_DOCGEN_ACCOUNT_ID
-      }.r2.cloudflarestorage.com/${encodeURIComponent(objectKey)}`;
-      console.log("Public URL:", publicUrl);
-    })
-    .catch(function (error) {
-      console.error(error);
-    });
-  // const fs = require("fs");
-  // const image = fs.readFileSync("./output.html");
-  // const blob = new Blob([image]);
-
-  // const formData = new FormData();
-  // formData.append("file", blob, "filename_for_cloudflare");
-
-  // try {
-  //   const response = await fetch(
-  //     `https://api.cloudflare.com/client/v4/accounts/${process.env.R2_DOCGEN_ACCOUNT_ID}/r2/buckets/docgen-backend`,
-  //     {
-  //       method: "POST",
-  //       headers: {
-  //         Authorization: `Bearer ${process.env.R2_AUTH_TOKEN}`,
-  //       },
-  //       body: formData,
-  //     }
-  //   );
-  //   // Your image has been uploaded
-  //   // Do something with the response, e.g. save image ID in a database
-  //   console.log(await response.json());
-  // } catch (error) {
-  //   console.error(error);
-  // }
   res.json({
     status: 200,
     message: "API is working properly",
