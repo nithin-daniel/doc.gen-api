d2411e2e7c9203ef1c783d6f6cca27d5d8f78925
Nithin Daniel <nithindaniel2018@gmail.com>
2024-08-22 15:03:20 +0530

    fix:add cors

--- server.js
+++ server.js
@@ -18,6 +18,16 @@ app.use(express.urlencoded({ extended: true }));
 
 const multer = require("multer");
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Allow all origins or specify your domain
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200); // Preflight request handling
+  }
+  next();
+});
+
 app.use("/event_photo", express.static("event_photos_uploads"));
 // app.use("/pdf", express.static("output"));
 
